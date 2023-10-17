d583f79e54e5a827c7faeed3ba5a1c5277a4e2b3
mortadha1144 <mortadha114@.gmail.com>
2023-10-18 00:30:10 +0300

    add getAspectRatio extension

---
+++ lib/state/image_upload/models/image_with_aspect_ratio.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+@immutable
+class ImageWithAspectRatio {
+  final Image image;
+  final double aspectRatio;
+
+  const ImageWithAspectRatio({
+    required this.image,
+    required this.aspectRatio,
+  });
+}
