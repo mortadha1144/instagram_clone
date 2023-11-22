905b17f6ca856f9e281f20e8b859f6f658156d0c
mortadha1144 <mortadha114@.gmail.com>
2023-11-22 10:16:33 +0300

    finish delete post state notifier

---
+++ lib/state/posts/notfiers/delete_post_state_notifier.dart
@@ -0,0 +1,95 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instagram_clone/state/constants/firebase_collection_name.dart';
+import 'package:instagram_clone/state/constants/firebase_field_name.dart';
+import 'package:instagram_clone/state/image_upload/extensions/get_collection_name_from_file_type.dart';
+import 'package:instagram_clone/state/image_upload/typedefs/is_loading.dart';
+import 'package:instagram_clone/state/posts/models/post.dart';
+import 'package:instagram_clone/state/posts/typedefs/post_id.dart';
+
+class DeletePostStateNotifier extends StateNotifier<IsLoading> {
+  DeletePostStateNotifier() : super(false);
+  set isLoading(bool value) => state = value;
+
+  Future<bool> deletePost({
+    required Post post,
+  }) async {
+    isLoading = true;
+
+    try {
+      // delete the post's thumbnail
+
+    await FirebaseStorage.instance
+        .ref()
+        .child(post.postId)
+        .child(FirebaseCollectionName.thumbnails)
+        .child(post.thumbnailStorageId)
+        .delete();
+
+    // delete the post's original (video or image)
+    await FirebaseStorage.instance
+        .ref()
+        .child(post.postId)
+        .child(post.fileType.collectionName)
+        .child(post.originalFileStorageId)
+        .delete();
+
+    // delete all comments associated with the post
+
+    await _deleteAllDocuments(
+      collectionName: FirebaseCollectionName.comments,
+      postId: post.postId,
+    );
+
+    // delete all like associated with the post
+
+    await _deleteAllDocuments(
+      collectionName: FirebaseCollectionName.likes,
+      postId: post.postId,
+    );
+
+    // finally delete the post itself 
+
+    final postInCollection = await FirebaseFirestore.instance
+        .collection(FirebaseCollectionName.posts)
+        .where(FirebaseFieldName.postId, isEqualTo: post.postId)
+        .limit(1)
+        .get();
+
+        for (final post in postInCollection.docs) {
+          await post.reference.delete();
+        }
+      return true;
+    } catch (_) {
+      return false;
+    } finally {
+      isLoading = false;
+    }
+
+    
+  }
+
+  Future<void> _deleteAllDocuments({
+    required PostId postId,
+    required String collectionName,
+  }) {
+    return FirebaseFirestore.instance.runTransaction(
+      maxAttempts: 3,
+      timeout: const Duration(seconds: 20),
+      (transaction) async {
+        final query = await FirebaseFirestore.instance
+            .collection(collectionName)
+            .where(
+              FirebaseFieldName.postId,
+              isEqualTo: postId,
+            )
+            .get();
+
+        for (final doc in query.docs) {
+          transaction.delete(doc.reference);
+        }
+      },
+    );
+  }
+}
